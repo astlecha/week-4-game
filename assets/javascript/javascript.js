9a2779b7bc442a8e23775ab27b59510e9f226a00
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-06-17 11:17:13 -0700

    creates js functions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,19 +1,46 @@
 var score = 0;
 var wins = 0;
-var gems = ["ruby",];
-var gemObject = {}
+var crystals = ["ruby", "diamond", "citrine", "emerald"];
+var crystalObject = {}
 
 var gamePlay{
 	
 }
 
 //computer picks random number
-var generateRandomGemValue = function(){
+var genRandCrysVal = function(){
 	for(var.....){
 		//pick numbers 19-120
-		gemObject(gems[i])=Math.floor(Math.random())
+		crystalObject(crystals[i])=Math.floor(Math.random())
 	}
 }
 
+//create crystals with different random numbers
+function ruby(){
+	Math.floor((Math.random() * 10) + 1);
+	score+(numberrrrr)
+}
+function diamond(){
+	Math.floor((Math.random() * 10) + 1);
+}
+function citrine(){
+	Math.floor((Math.random() * 10) + 1);
+}
+function emerald(){
+	Math.floor((Math.random() * 10) + 1);
+}
+
+//score adds the value of each crystal together. make add function?
+
 //reset losses, generate points, call another random goal
-function resetGame()
+function resetGame(){
+	//if they win increase wins
+	if(score =genRandCrysVal){
+
+	}
+	//else if they lose, increase losses
+	if(score>random)
+
+	genRandCrysVal();
+
+}
