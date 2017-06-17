a3df8ce4449599aac32803a275cf21725fde76bb
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-06-17 10:34:00 -0700

    adds instructions

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,18 @@
+var score = 0;
+var wins = 0;
+var gems = ["ruby",];
+var gemObject = {}
+
+var gamePlay{
+	
+}
+
+var generateRandomGemValue = function(){
+	for(var.....){
+		//pick numbers 19-120
+		gemObject(gems[i])=Math.floor(Math.random())
+	}
+}
+
+//reset losses, generate points, call another random goal
+function resetGame()
