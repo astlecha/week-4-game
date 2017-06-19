285303077a89c7778380ec2fac921b8d28be42b7
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-06-19 16:25:46 -0700

    changes css styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,96 +2,115 @@ var score = 0;
 var wins = 0;
 var losses = 0;
 var targetNumber = (Math.floor(Math.random()*120)+19);
-var increment = [];
+var buttonNumArray = [];
 
 
 //call on all functions in the document
 $(document).ready(function(){
 
+//generate random number for each jewel
+function crystalNumbers(){
+  var randomCrysNum = "";
+  for (var i = 0; i < 4; i++) {
+    if (i > -1) {
+      randomCrysNum = Math.floor((Math.random()*12)+1);
+    };
+    buttonNumArray.push(randomCrysNum);
+  };
+}
+
+//generate random jewel numbers
+crystalNumbers();
+console.log(buttonNumArray);
+
 //call on targetNumber
   $("#random-number").text(targetNumber);
 
 //pick random number for target and reset score to 0
-function resetGame(){
+function reset(){
   targetNumber=Math.floor(Math.random()*120+19);
-      console.log(targetNumber);
+    console.log(targetNumber);
+    $('#random-number').text(targetNumber);
 
-  $('#random-number').text(targetNumber);
-      number1= Math.floor((Math.random()*11)+1);
-      number2= Math.floor((Math.random()*11)+1);
-      number3= Math.floor((Math.random()*11)+1);
-      number4= Math.floor((Math.random()*11)+1);
-      score= 0;
-      $('#current-score').text(score);
+    //reset the score
+    score= 0;
+    $('#current-score').text(score);
+
+    //reset button values
+    buttonNumArray = [];
+    crystalNumbers();
+    console.log(buttonNumArray);
 };
 
 
 //call on when user wins
 function winner(){
+    alert("You win!!! Play again?");
     wins++;
-    $("#userWins").text(wins);
+    $("#userWins").text("Wins: "+wins);
     reset();
 }
 
 //call on this function when user loses
 function loser(){
+    alert("You lose :( Try again?");
     losses++;
-    $("#userLoses").text(loses);
+    $("#userLoses").text("Losses: "+losses);
     reset();
 }
 
 
-//increment score by individual random numbers on click.
-$("#ruby-image").on("click", function() {
-  score += number1;
-  alert("Your new score is: " + score);
-//call on winner or loser function  
-  if (score === targetNumber) {
-    winner();
-  }
-  else if (score > targetNumber) {
-    loser();
-  }
-});
+  //increment score by individual random numbers on click.
+  $("#ruby-image").on("click", function() {
+    score += buttonNumArray[0];
+    $("#current-score").text(score);
+  //call on winner or loser function  
+    if (score === targetNumber) {
+      winner();
+    }
+    else if (score > targetNumber) {
+      loser();
+    }
+  });
 
 
-$("#diamond-image").on("click", function() {
-  score += number2;
-  alert("Your new score is: " + score);
-//call on winner or loser function
-  if (score === targetNumber) {
-    winner();
-  }
-  else if (score > targetNumber) {
-    loser();
-  }  
-});
+  $("#diamond-image").on("click", function() {
+    score += buttonNumArray[1];
+    $("#current-score").text(score);
+  //call on winner or loser function
+    if (score === targetNumber) {
+      winner();
+    }
+    else if (score > targetNumber) {
+      loser();
+    }  
+  });
 
 
-$("#citrine-image").on("click", function() {
-  score += number3;
-  alert("Your new score is: " + score);
-//call on winner or loser function
-  if (score === targetNumber) {
-    winner();
-  }
-  else if (score > targetNumber) {
-    loser();
-  } 
-});
+  $("#citrine-image").on("click", function() {
+    score += buttonNumArray[2];
+    $("#current-score").text(score);
+  //call on winner or loser function
+    if (score === targetNumber) {
+      winner();
+    }
+    else if (score > targetNumber) {
+      loser();
+    } 
+  });
 
 
-$("#emerald-image").on("click", function() {
-  score += number4;
-  alert("Your new score is: " + score);
-//call on winner or loser function
-  if (score === targetNumber) {
-    winner();
-  }
-  else if (score > targetNumber) {
-    loser();
-  }  
-});
+  $("#emerald-image").on("click", function() {
+    score += buttonNumArray[3];
+    $("#current-score").text(score);
+  //call on winner or loser function
+    if (score === targetNumber) {
+      winner();
+    }
+    else if (score > targetNumber) {
+      loser();
+    }  
+  });
 
 
 })
